d120cc6e87988def131a9c3a58c579759fcfb0d6
Hemanth Anumolu <sp2013hem@gmail.com>
2021-10-24 15:13:26 +0200

    add table

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { StatsComponent } from './stats/stats.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [HomeComponent, StatsComponent],
+  declarations: [HomeComponent, StatsComponent, TableComponent],
   imports: [SharedModule, HomeRoutingModule],
 })
 export class HomeModule {}
