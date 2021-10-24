9999c3ef93f9b66e0bc10df66a780e87670425b7
Hemanth Anumolu <sp2013hem@gmail.com>
2021-10-24 02:00:07 +0200

    add firebase modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAnalyticsModule } from '@angular/fire/compat/analytics';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { StocksAPI } from './core/services/stocks.service';
 import { SideNavModule } from './shared/modules/side-nav/side-nav.module';
@@ -9,7 +13,15 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule, SideNavModule, StatsModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
+    SharedModule,
+    SideNavModule,
+    StatsModule,
+  ],
   providers: [StocksAPI],
   bootstrap: [AppComponent],
 })
